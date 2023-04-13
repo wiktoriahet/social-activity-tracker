0f4a064e8de94d96805a512d7b81aa7d78a64931
wiktoriahet <wiktoriaspyc@gmail.com>
2023-04-13 11:12:34 +0200

    added id as primary key

--- src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/model/ActivityTypeModel.java
+++ src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/model/ActivityTypeModel.java
@@ -2,8 +2,17 @@ package pl.hetman.wiktoria.java.app.socialactivitytracker.model;
 
 public class ActivityTypeModel {
 
+    private Long id;
     private String name;
     private boolean custom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -19,5 +28,14 @@ public class ActivityTypeModel {
 
     public void setCustom(boolean custom) {
         this.custom = custom;
+    }
+
+    @Override
+    public String toString() {
+        return "ActivityTypeModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", custom=" + custom +
+                '}';
     }
 }
