7c3765fc9a5c4cb098d186951b59c6124de6426e
wiktoriahet <wiktoriaspyc@gmail.com>
2023-06-20 18:25:18 +0200

    added CRUD methods for services

--- src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/service/ActivityTypeService.java
+++ src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/service/ActivityTypeService.java
@@ -1,5 +1,39 @@
 package pl.hetman.wiktoria.java.app.socialactivitytracker.service;
 
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityTypeModel;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.dao.ActivityDao;
+
+import java.util.List;
+import java.util.logging.Logger;
+
 public class ActivityTypeService {
-    //crud tu
+    private static final Logger LOGGER = Logger.getLogger(ActivityTypeService.class.getName());
+
+    private final ActivityDao activityDao; //zależność
+
+    public ActivityTypeService(ActivityDao activityDao) { //wstrzykiwanie zależności za pomocą konstr.
+        this.activityDao = activityDao;
+    }
+
+
+    // C - create
+    public ActivityTypeModel create(ActivityTypeModel activityTypeModel) {
+        return null;
+    }
+    // R - read
+    public ActivityTypeModel read(Long id) {
+        return null;
+    }
+    // U - update
+    public ActivityTypeModel update(Long id) {
+        return null;
+    }
+    // D - delete
+    public void delete(Long id) {
+    }
+    // L - list
+    public List<ActivityTypeModel> list() {
+        return null;
+    }
+
 }
--- src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/service/UserService.java
+++ src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/service/UserService.java
@@ -1,5 +1,38 @@
 package pl.hetman.wiktoria.java.app.socialactivitytracker.service;
 
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityTypeModel;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.UserModel;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.dao.ActivityDao;
+
+import java.util.List;
+import java.util.logging.Logger;
+
 public class UserService {
-    //crud tu
+    private static final Logger LOGGER = Logger.getLogger(UserService.class.getName());
+
+    private final ActivityDao activityDao; //zależność
+
+    public UserService(ActivityDao activityDao) { //wstrzykiwanie zależności za pomocą konstr.
+        this.activityDao = activityDao;
+    }
+
+    // C - create
+    public UserModel create(UserModel userModel) {
+        return null;
+    }
+    // R - read
+    public UserModel read(Long id) {
+        return null;
+    }
+    // U - update
+    public UserModel update(Long id) {
+        return null;
+    }
+    // D - delete
+    public void delete(Long id) {
+    }
+    // L - list
+    public List<UserModel> list() {
+        return null;
+    }
 }
