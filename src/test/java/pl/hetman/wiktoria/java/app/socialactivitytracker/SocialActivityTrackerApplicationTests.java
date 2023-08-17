4faf85b0bc6efd3b7705c2df45300a1579384bcb
wiktoriahet <wiktoriaspyc@gmail.com>
2023-08-17 11:34:58 +0200

    Added Spring configuration, added main for Spring with tests

---
+++ src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/SocialActivityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hetman.wiktoria.java.app.socialactivitytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialActivityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
