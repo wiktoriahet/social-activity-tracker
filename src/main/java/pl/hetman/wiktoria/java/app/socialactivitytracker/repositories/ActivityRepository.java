acba55e9d32e71a2ba355841fa55ffc2636230c5
wiktoriahet <wiktoriaspyc@gmail.com>
2023-09-05 14:50:16 +0200

    Added shell repositories, created integrated tests for existing repositories

--- src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/repositories/ActivityRepository.java
+++ src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/repositories/ActivityRepository.java
@@ -34,51 +34,51 @@ public class ActivityRepository {
         return activityModel;
     }
 
-//    public ActivityModel read(Long id) {
-//        LOGGER.info("read(" + id + ")");
-//        Session session = SESSION_FACTORY.openSession();
-//        session.getTransaction().begin();
-//        ActivityModel activityModel = session.get(ActivityModel.class, id);
-//        session.getTransaction().commit();
-//        LOGGER.info("read(...) = " + activityModel);
-//
-//        return activityModel;
-//    }
+    public ActivityModel read(Long id) {
+        LOGGER.info("read(" + id + ")");
+        Session session = SESSION_FACTORY.openSession();
+        session.getTransaction().begin();
+        ActivityModel activityModel = session.get(ActivityModel.class, id);
+        session.getTransaction().commit();
+        LOGGER.info("read(...) = " + activityModel);
 
-//    public ActivityModel update(ActivityModel activityModel) {
-//        LOGGER.info("update(" + activityModel + ")");
-//        Session session = SESSION_FACTORY.openSession();
-//        session.getTransaction().begin();
-//        session.merge(activityModel);
-//        session.getTransaction().commit();
-//        LOGGER.info("update(...) = " + activityModel);
-//
-//        return activityModel;
-//    }
+        return activityModel;
+    }
 
-//    public boolean delete(ActivityModel activityModel) {
-//        LOGGER.info("delete(" + activityModel + ")");
-//        boolean deleted;
-//
-//        Session session = SESSION_FACTORY.openSession();
-//        try {
-//            session.getTransaction().begin();
-//            session.remove(activityModel);
-//            session.getTransaction().commit();
-//
-//            deleted = true;
-//            return deleted;
-//        } catch (HibernateException e) {
-//            LOGGER.log(Level.SEVERE, "Error while deleting Activity", e);
-//            deleted = false;
-//            session.getTransaction().rollback();
-//        } catch (Exception e) {
-//            LOGGER.log(Level.SEVERE, "Error while deleting Activity", e);
-//            deleted = false;
-//            session.getTransaction().rollback();
-//        }
-//        LOGGER.info("delete(...) = " + activityModel);
-//        return deleted;
-//    }
+    public ActivityModel update(ActivityModel activityModel) {
+        LOGGER.info("update(" + activityModel + ")");
+        Session session = SESSION_FACTORY.openSession();
+        session.getTransaction().begin();
+        session.merge(activityModel);
+        session.getTransaction().commit();
+        LOGGER.info("update(...) = " + activityModel);
+
+        return activityModel;
+    }
+
+    public boolean delete(ActivityModel activityModel) {
+        LOGGER.info("delete(" + activityModel + ")");
+        boolean deleted;
+
+        Session session = SESSION_FACTORY.openSession();
+        try {
+            session.getTransaction().begin();
+            session.remove(activityModel);
+            session.getTransaction().commit();
+
+            deleted = true;
+            return deleted;
+        } catch (HibernateException e) {
+            LOGGER.log(Level.SEVERE, "Error while deleting Activity", e);
+            deleted = false;
+            session.getTransaction().rollback();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error while deleting Activity", e);
+            deleted = false;
+            session.getTransaction().rollback();
+        }
+        LOGGER.info("delete(...) = " + activityModel);
+        return deleted;
+    }
 
 }
--- src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/repositories/ActivityRepositoryTest.java
+++ src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/repositories/ActivityRepositoryTest.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityModel;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class ActivityRepositoryTest {
     private static final String ACTIVITY_LABEL_RUNNING = "Running";
-    public static final String ACTIVITY_LABEL_KAYAKING = "Kayaking";
+    public static final String ACTIVITY_LABEL_TENNIS = "Playing tennis";
 
     public ActivityRepositoryTest() {
     }
@@ -23,15 +21,13 @@ class ActivityRepositoryTest {
     void create() {
         //given
         ActivityModel activityModel = new ActivityModel();
-        activityModel.setLabel(ACTIVITY_LABEL_KAYAKING);
-
+        activityModel.setLabel(ACTIVITY_LABEL_TENNIS);
 
         //when
         ActivityModel createdActivityModel = activityRepository.create(activityModel);
 
         //then
         Assertions.assertNotNull(createdActivityModel, "createdActivityModel is null");
-
 
     }
 }
