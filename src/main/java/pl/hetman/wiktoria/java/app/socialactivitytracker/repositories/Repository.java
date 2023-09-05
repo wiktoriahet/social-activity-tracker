56429446ce4f42b1b1c6d3fdc5e5c13c749fe11a
wiktoriahet <wiktoriaspyc@gmail.com>
2023-09-05 15:17:53 +0200

    Refactored code for Optionals in repositories, refactored tests for Optionlas, added repository interface, implemented interface in repositories

---
+++ src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/repositories/Repository.java
@@ -0,0 +1,20 @@
+package pl.hetman.wiktoria.java.app.socialactivitytracker.repositories;
+
+import pl.hetman.wiktoria.java.app.socialactivitytracker.api.exception.ActivityException;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository <T>{
+
+    Optional<T> create(T t) throws ActivityException;
+
+    Optional<T> read(Long id) throws ActivityException;
+
+    void update(T t) throws ActivityException;
+
+    void delete(T t) throws ActivityException;
+
+    List<T> list ()throws ActivityException;
+
+}
