e7e05c8b116dea3e0fcc9c1813f036699fa7892e
wiktoriahet <wiktoriaspyc@gmail.com>
2023-09-18 11:36:45 +0200

    generated test for new repository

---
+++ src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/UserJpaDataRepositoryTest.java
@@ -0,0 +1,10 @@
+package pl.hetman.wiktoria.java.app.socialactivitytracker.dao.datajpa.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserJpaDataRepositoryTest {
+
+}
