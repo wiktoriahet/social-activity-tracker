b0cdc439dff52c75bcb0666345c4f81379c26af6
wiktoriahet <wiktoriaspyc@gmail.com>
2023-09-18 11:35:55 +0200

    created jpa repository for user

---
+++ src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/UserJpaDataRepository.java
@@ -0,0 +1,7 @@
+package pl.hetman.wiktoria.java.app.socialactivitytracker.dao.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserJpaDataRepository {
+}
