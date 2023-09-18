9156394d179cdb90000cdb6e6ff3eec8a9360ad6
wiktoriahet <wiktoriaspyc@gmail.com>
2023-09-18 11:03:02 +0200

    tested create method for jpa repository

--- src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/ActivityTypeDataJpaRepositoryTest.java
+++ src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/ActivityTypeDataJpaRepositoryTest.java
@@ -1,23 +1,34 @@
 package pl.hetman.wiktoria.java.app.socialactivitytracker.dao.datajpa.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityTypeModel;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ActivityTypeDataJpaRepositoryTest {
 
-    private static final String ACTIVITY_LABEL_FISHING = "Fishing";
-    private static final String ACTIVITY_LABEL_SNORKELING = "Snorkeling";
+    private static final String ACTIVITY_NAME_FISHING = "Fishing";
+    private static final String ACTIVITY_NAME_SNORKELING = "Snorkeling";
 
     @Autowired
     private ActivityTypeDataJpaRepository activityTypeDataJpaRepository;
 
     @Test
     public void create() {
+        //given
+        ActivityTypeModel activityTypeModel = new ActivityTypeModel();
+        activityTypeModel.setCustom(true);
+        activityTypeModel.setName(ACTIVITY_NAME_SNORKELING);
 
+        //when
+        ActivityTypeModel savedActivityTypeModel = activityTypeDataJpaRepository.save(activityTypeModel);
+
+        //then
+        Assertions.assertNotNull(savedActivityTypeModel, "savedActivityTypeModel is null");
 
     }
 
