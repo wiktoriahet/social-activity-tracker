836f275f88eb03d0ff67dc6891bd7ca78591f624
wiktoriahet <wiktoriaspyc@gmail.com>
2023-09-18 11:44:23 +0200

    testes create method for user jpa repository

--- src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/UserJpaDataRepositoryTest.java
+++ src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/UserJpaDataRepositoryTest.java
@@ -1,7 +1,12 @@
 package pl.hetman.wiktoria.java.app.socialactivitytracker.dao.datajpa.repository;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityModel;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityTypeModel;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.UserModel;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,8 +17,22 @@ class UserJpaDataRepositoryTest {
     private static final String ACTIVITY_NAME_SNORKELING = "Snorkeling";
 
     @Autowired
-    private UserJpaDataRepository activityTypeDataJpaRepository;
+    private UserJpaDataRepository userJpaDataRepository;
 
+    @Test
+    public void create() {
+        //given
+        UserModel userModel = new UserModel();
+//        ActivityModel activityModel = new ActivityModel();
+//        activityModel.setLabel(ACTIVITY_NAME_SNORKELING);
+//        userModel.setActivityModel(activityModel);
+        //hibernate onetoone!!!!
 
+        //when
+        UserModel savedUserModel = userJpaDataRepository.save(userModel);
 
+        //then
+        Assertions.assertNotNull(savedUserModel, "savedUserModel is null");
+
+    }
 }
