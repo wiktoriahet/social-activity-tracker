984051369cc07359901b5da45068f5685dc6a114
wiktoriahet <wiktoriaspyc@gmail.com>
2023-09-18 11:38:17 +0200

    added static fields and dependency in generated test

--- src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/UserJpaDataRepositoryTest.java
+++ src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/UserJpaDataRepositoryTest.java
@@ -1,10 +1,19 @@
 package pl.hetman.wiktoria.java.app.socialactivitytracker.dao.datajpa.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserJpaDataRepositoryTest {
+
+    private static final String ACTIVITY_NAME_FISHING = "Fishing";
+    private static final String ACTIVITY_NAME_SNORKELING = "Snorkeling";
+
+    @Autowired
+    private UserJpaDataRepository activityTypeDataJpaRepository;
+
+
 
 }
