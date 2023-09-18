6ab2fa8e3b0993f7fa6f9c43b2751b9cfcba08ca
wiktoriahet <wiktoriaspyc@gmail.com>
2023-09-18 10:53:12 +0200

    generated test for jpa repository

--- src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/ActivityTypeDataJpaRepository.java
+++ src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/ActivityTypeDataJpaRepository.java
@@ -2,7 +2,6 @@ package pl.hetman.wiktoria.java.app.socialactivitytracker.dao.datajpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityModel;
 import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityTypeModel;
 
 @Repository
---
+++ src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/ActivityTypeDataJpaRepositoryTest.java
@@ -0,0 +1,7 @@
+package pl.hetman.wiktoria.java.app.socialactivitytracker.dao.datajpa.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityTypeDataJpaRepositoryTest {
+
+}
