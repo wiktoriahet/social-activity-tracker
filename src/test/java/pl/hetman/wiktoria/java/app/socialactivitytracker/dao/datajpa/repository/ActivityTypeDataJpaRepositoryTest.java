0ab4349c8627cad470ea95665787a190c45cecc3
wiktoriahet <wiktoriaspyc@gmail.com>
2023-09-18 10:58:35 +0200

    added static fields in jpa repository test, added shell test methods

--- src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/ActivityTypeDataJpaRepositoryTest.java
+++ src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/ActivityTypeDataJpaRepositoryTest.java
@@ -1,7 +1,38 @@
 package pl.hetman.wiktoria.java.app.socialactivitytracker.dao.datajpa.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ActivityTypeDataJpaRepositoryTest {
 
+    private static final String ACTIVITY_LABEL_FISHING = "Fishing";
+    private static final String ACTIVITY_LABEL_SNORKELING = "Snorkeling";
+
+    @Autowired
+    private ActivityTypeDataJpaRepository activityTypeDataJpaRepository;
+
+    @Test
+    public void create() {
+
+
+    }
+
+    @Test
+    public void read(){
+
+    }
+
+    @Test
+    public void update(){
+
+    }
+
+    @Test
+    public void delete(){
+
+    }
 }
