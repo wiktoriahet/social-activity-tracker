11da173da360b8951845660476158e3cf5a8a0e7
wiktoriahet <wiktoriaspyc@gmail.com>
2023-10-02 11:23:16 +0200

    generated jpa integration test for user

--- src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/controller/model/UserModel.java
+++ src/main/java/pl/hetman/wiktoria/java/app/socialactivitytracker/controller/model/UserModel.java
@@ -1,11 +1,13 @@
 package pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,9 +18,17 @@ public class UserModel {
     @GeneratedValue
     private Long id;
 
-    @OneToMany
-//    @JoinColumn(name = "user_model")
-    private Set<ActivityModel> activities;
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
+//    @JoinColumn(name = "USER_ID")
+    private Set<ActivityModel> activities = new HashSet<>();
+
+    public void addActivity(ActivityModel activityModel) {
+        this.activities.add(activityModel);
+        activityModel.setUser(this);
+    }
 
     public Long getId() {
         return id;
@@ -26,6 +36,22 @@ public class UserModel {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Set<ActivityModel> getActivities() {
---
+++ src/test/java/pl/hetman/wiktoria/java/app/socialactivitytracker/dao/datajpa/repository/UserJpaDataRepositoryIntegrationTest.java
@@ -0,0 +1,73 @@
+package pl.hetman.wiktoria.java.app.socialactivitytracker.dao.datajpa.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityModel;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.ActivityTypeModel;
+import pl.hetman.wiktoria.java.app.socialactivitytracker.controller.model.UserModel;
+
+@SpringBootTest
+class UserJpaDataRepositoryIntegrationTest {
+
+    private static final String ACTIVITY_NAME_FISHING = "Fishing";
+    private static final String ACTIVITY_NAME_SNORKELING = "Snorkeling";
+
+    @Autowired
+    private UserJpaDataRepository userJpaDataRepository;
+
+    @Test
+//    @Transactional
+//    @Rollback(value = false)
+    public void create() {
+        //given
+        ActivityTypeModel fishingActivityTypeModel = new ActivityTypeModel();
+        fishingActivityTypeModel.setName(ACTIVITY_NAME_FISHING);
+
+        ActivityModel fishingActivityModel = new ActivityModel();
+        fishingActivityModel.chooseActivityType(fishingActivityTypeModel);
+        fishingActivityModel.setLabel("Karp");
+
+        UserModel userModel = new UserModel();
+        userModel.setUsername("root");
+//        userModel.setActivities(Set.of(fishingActivityModel));
+        userModel.addActivity(fishingActivityModel);
+
+        //when
+        UserModel savedUserModel = userJpaDataRepository.save(userModel);
+
+        //then
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(savedUserModel, "savedUserModel is null"),
+                () -> Assertions.assertNotNull(savedUserModel.getId(), "savedUserModel ID is null"),
+                () -> Assertions.assertEquals(1, savedUserModel.getActivities().size())
+        );
+    }
+}
+/*
+SELECT *
+FROM ACTIVITIES A
+JOIN USERS U
+ON A.USER_ID = U.ID
+JOIN ACTIVITY_TYPES AT
+ON A.ACTIVITY_TYPE_ID =AT.ID;
+
+--
+
+SELECT *
+FROM ACTIVITIES A
+JOIN USERS_ACTIVITIES UA
+ON A.ID = UA.ACTIVITIES_ID
+JOIN USERS U
+ON U.ID = UA.USERMODEL_ID
+JOIN ACTIVITY_TYPES AT
+ON A.ACTIVITY_TYPE_ID =AT.ID;
+
+----
+
+drop table users_activities ;
+drop table activities ;
+drop table activity_types ;
+drop table users ;
+ */
